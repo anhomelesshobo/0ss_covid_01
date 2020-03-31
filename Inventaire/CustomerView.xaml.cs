c0fab7a02a5f2f01c064c5e8dc47f54f67fb31df
Pierluc Dupont <pierlucdupont01@hotmail.ca>
2020-03-31 16:52:53 -0400

    Implémentation du model MVVM dans le code

---
+++ Inventaire/CustomerView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using BillingManagement.Models;
+using BillingManagement.UI.ViewModels;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BillingManagement.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow(CustomersViewModels vm)
+        {
+            InitializeComponent();
+
+            DataContext = vm;
+        }
+        private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+
+           
+            new CustomersViewModels = 
+        }
+    }
+}
---
+++ Inventaire/ViewModels/CustomersViewModels.cs
@@ -0,0 +1,46 @@
+﻿using BillingManagement.Business;
+using BillingManagement.Models;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace BillingManagement.UI.ViewModels
+{
+   public class CustomersViewModels : BaseViewModels
+    {
+        CustomersDataService customersDataService = new CustomersDataService();
+
+        private ObservableCollection<Customer> customers;
+        private Customer selectedCustomer;
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => customers;
+            private set
+            {
+                customers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Customer SelectedCustomer
+        {
+            get => selectedCustomer;
+            set
+            {
+                selectedCustomer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CustomersViewModels()
+        {
+            InitValues();
+        }
+
+        private void InitValues()
+        {
+            Customers = new ObservableCollection<Customer>(customersDataService.GetAll());
+            Debug.WriteLine(Customers.Count);
+        }
+    }
+}
